44c0d7d27328ccd3e810a952b605833b6f33b279
Rajatyadav <yadupatirajat@gmail.com>
2025-09-08 23:31:06 +0530

    icon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'RAJSME',
+  // description: 'Created with v0',
+  // generator: 'v0.dev',
+  icons: {
+    icon: '/logoo.png',
+  },
 }
 
 export default function RootLayout({
